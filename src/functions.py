2ae3827dfc883c4e80ac71ce49d859a18e17ec43
Kevin Tran <kevinqt@Kevins-MacBook-Air.local>
2023-11-12 23:57:15 -0500

    final

--- src/functions.py
+++ src/functions.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
+## Plot the output class percentage of instances for each datasets
 def plot(dataset, filename, output_class):
     class_distribution = dataset[output_class].value_counts(normalize=True) * 100  # Convert to percentage
 
@@ -25,60 +26,77 @@ def plot(dataset, filename, output_class):
     axes.yaxis.set_major_formatter(mtick.PercentFormatter())  # Format y-axis as percentages
     axes.set_ylim(0, 50)  # Set the maximum value to 50%
 
+    # Save the plot
     plt.savefig('images/' + filename + '-classes.png')
     plt.clf()
 
-def base_dt(file_path, image_path, X_train, X_test, y_train, y_test, iteration):
+## Function to define the Base-DT model
+def base_dt(file_path, image_path, abalone, X_train, X_test, y_train, y_test, iteration):
+    # Initialize Decision Tree
     dt = DecisionTreeClassifier()
+    # Train decision tree
     dt.fit(X_train, y_train)
 
+    # Visualize the decision tree
     if iteration == 0:
         plt.figure(figsize=(20, 10))
-        if image_path == 'graphs/base_dt_abalone.png':
-            plot_tree(dt, feature_names=X_train.columns, class_names=list(map(str, dt.classes_)), filled=True, rounded=True, max_depth=5)
-        plot_tree(dt, feature_names=X_train.columns, class_names=list(map(str, dt.classes_)), filled=True, rounded=True)
+        if abalone:
+            plot_tree(dt, feature_names=X_train.columns, class_names=list(map(str, dt.classes_)), filled=True, rounded=True, max_depth=4, fontsize=6)
+        else:
+            plot_tree(dt, feature_names=X_train.columns, class_names=list(map(str, dt.classes_)), filled=True, rounded=True)
         plt.savefig(image_path, format='png')
         plt.show()
 
+    # Test the decision tree
     y_pred = dt.predict(X_test)
 
+    # Returns the average and variance metrics for this model
     return evaluate(y_test, y_pred, dt, iteration, file_path, 'Base-DT', dt)
 
-def top_dt(file_path, image_path, X_train, X_test, y_train, y_test, iteration):
+## Function to define the Top-DT model
+def top_dt(file_path, image_path, abalone, X_train, X_test, y_train, y_test, iteration):
+    # Initialize the parameters for this model
     top_dt_params = {
         'criterion': ['gini', 'entropy'],
         'max_depth': [10, 20, None],  
         'min_samples_split': [2, 4, 6]
     }
+    # Initialize Decision Tree
     dt = DecisionTreeClassifier(random_state=0)
+    # Initialize Grid Search and train it
     top_dt_grid = GridSearchCV(dt, top_dt_params, cv=5, scoring='accuracy', n_jobs=-1)
     top_dt_grid.fit(X_train, y_train)
 
+    # Get the model with the best hyperparameters and train it
     best_dt = top_dt_grid.best_estimator_
     best_dt.fit(X_train, y_train)
 
     # Visualize the decision tree
     if iteration == 0:
         plt.figure(figsize=(20, 10))  # Set to a large figure size to make the tree readable
-        if image_path == 'graphs/base_dt_abalone.png':
-            plot_tree(best_dt, filled=True, feature_names=X_train.columns, class_names=True, max_depth=5)
+        if abalone:
+            plot_tree(best_dt, filled=True, feature_names=X_train.columns, class_names=True, max_depth=5, fontsize=6)
         else:
             plot_tree(best_dt, filled=True, feature_names=X_train.columns, class_names=True)
-            plt.title('Best Decision Tree found with GridSearch')
-            plt.savefig(image_path, format='png')  # Save the figure to a file
-            plt.show()
+        plt.title('Best Decision Tree found with GridSearch')
+        plt.savefig(image_path, format='png')  # Save the figure to a file
+        plt.show()
 
+    # Test the decision tree
     y_pred = best_dt.predict(X_test)
 
+    # Returns the average and variance metrics for this model
     return evaluate(y_test, y_pred, dt, iteration, file_path, 'Top-DT', best_dt)
 
+## Function to define the Base-MLP model
 def base_mlp(file_path, X_train, X_test, y_train, y_test, iteration):
-
+    # Initialize MLP
     mlp = MLPClassifier(
         hidden_layer_sizes=(100, 100),
         activation='logistic',
         solver='sgd',
-        random_state=0
+        random_state=0,
+        max_iter=10000
     )
 
     # Train the classifier
@@ -87,28 +105,35 @@ def base_mlp(file_path, X_train, X_test, y_train, y_test, iteration):
     # Make predictions on the test set
     y_pred = mlp.predict(X_test)
 
+    # Returns the average and variance metrics for this model
     return evaluate(y_test, y_pred, mlp, iteration, file_path, 'Base-MLP', mlp)
 
+## Function to define the Top-MLP model
 def top_mlp(file_path, X_train, X_test, y_train, y_test, iteration):
+    # Initialize the parameters for this model
     top_mlp_params = {
-        'hidden_layer_sizes': [(30, 50), (10, 10, 10)],
+        'hidden_layer_sizes': [(500, 500), (30, 30, 30)],
         'activation': ['logistic', 'tanh', 'relu'],
         'solver': ['adam', 'sgd'],
-        'max_iter': [2000]
+        'max_iter': [500]
     }
+    # Initialize MLP
     mlp = MLPClassifier(random_state=0)
-    scoring_metrics = ['accuracy', 'precision_weighted', 'recall_weighted', 'f1_weighted']
+    # Initialize Grid Search and train it
     top_mlp_grid = GridSearchCV(mlp, top_mlp_params, cv=5)
     top_mlp_grid.fit(X_train, y_train)
 
+    # Get the model with the best hyperparameters and train it
     best_mlp = top_mlp_grid.best_estimator_
     best_mlp.fit(X_train, y_train)
 
     # Make predictions on the test set
     y_pred = best_mlp.predict(X_test)
 
+    # Returns the average and variance metrics for this model
     return evaluate(y_test, y_pred, mlp, iteration, file_path, 'Top-MLP', best_mlp)
 
+## This function evaluates the performace of the classification of a dataset
 def evaluate(y_test, y_pred, model, iteration, file_path, name, model_used):
     # Array to store performance metrics
     accuracy_results = []
@@ -125,6 +150,7 @@ def evaluate(y_test, y_pred, model, iteration, file_path, name, model_used):
     macro_f1 = f1_score(y_test, y_pred, average='macro')
     weighted_f1 = f1_score(y_test, y_pred, average='weighted')
 
+    # Store performance in a dictionary
     performance =  {
         'model': model,
         'confusion_matrix': conf_matrix,
@@ -134,15 +160,19 @@ def evaluate(y_test, y_pred, model, iteration, file_path, name, model_used):
         'weighted_f1': weighted_f1
     }
 
+    # Add the performance metric of the current iteration
     accuracy_results.append(performance['accuracy'])
     macro_f1_results.append(performance['macro_f1'])
     weighted_f1_results.append(performance['weighted_f1'])
 
+    # Only write the performance of the first iteration
     if iteration == 0:
         save_performance_to_file(file_path, name, model_used.get_params(), performance)
 
+    # Return the arrays of performance
     return accuracy_results, macro_f1_results, weighted_f1_results
     
+# Save the performance of a single iteration of a dataset to a file
 def save_performance_to_file(file_path, model_name, hyperparameters, performance_metrics):
     with open(file_path, 'a') as file:
         # Model name and Hyperparameters
@@ -172,6 +202,7 @@ def save_performance_to_file(file_path, model_name, hyperparameters, performance
         # Separator
         file.write("\n" + "*" * 50 + "\n\n")
 
+# Save the average and variance performance of all iteration of a dataset to a file
 def save_variance_to_file(file_path, model_name, accuracy_results, macro_f1_results, weighted_f1_results):
     with open(file_path, 'a') as file:
         # Average accuracy and variance
